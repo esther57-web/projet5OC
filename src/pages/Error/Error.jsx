192090636e50095538fbefaff5525e4caf6d9481
esther57-web <kamardine.ossete@gmail.com>
2024-04-23 23:02:52 +0200

    error page done

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,15 @@
+import './error.scss';
+import { Link } from 'react-router-dom';
+
 function Error() {
-    return <h1>Erreur 404</h1>
+    return (
+        <section className='error'>
+            <h1>404</h1>
+            <h2>Oups! La page que vous demandez n&apos;existe pas.</h2>
+            <Link to="/">Retourner sur la page d&apos;accueil</Link>
+        </section>
+        
+    )
 }
 
 export default Error
