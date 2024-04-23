410119cec479e3ba301990bb10e9c8063c55b384
esther57-web <kamardine.ossete@gmail.com>
2024-04-23 16:33:36 +0200

    header and sass initialization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import { Link } from 'react-router-dom';
+import '../Header/header.scss';
 
 function Header() {
-    return <div><Link to="/">accueil</Link><Link to="/annonces/:id">aller à annonces</Link></div>
+    return (
+    <header>
+        <div className='logo'>
+            <img src='./src/assets/desktop/icons/K.svg' alt='logo-K-element'></img>
+            <img src='./src/assets/desktop/icons/logo-desktop.svg' alt='logo-element'></img>
+            <img src='./src/assets/desktop/icons/S.svg' alt='logo-S-element'></img>
+            <img src='./src/assets/desktop/icons/A.svg' alt='logo-A-element'></img>
+        </div>
+        <nav>
+            <Link to="/">Accueil</Link>
+            <Link to="/apropos">A propos</Link>
+        </nav>
+    </header>
+    )
 }
 
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 //import React from 'react'
 import ReactDOM from 'react-dom/client'
 import RouterElem from './components/Router/Router'
+//import './style/main.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
