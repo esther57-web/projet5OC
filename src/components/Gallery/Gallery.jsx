c63006d9bb919c1ed47ca0a13fbe05684a1e99c1
esther57-web <kamardine.ossete@gmail.com>
2024-04-23 19:27:37 +0200

    home page done

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,19 @@
 import './Gallery.scss';
+import data from '../../data/logements.json'
+import { Link } from 'react-router-dom';
 
 function Gallery() {
+    
+    const announceList = data.map((announce) =>
+        <Link to={`/annonces/${announce.id}`} key={announce.id}>
+            <article style={{ backgroundImage: `url(${announce.cover})` }}>
+                <h2>{announce.title}</h2>
+            </article>
+        </Link>
+    );
     return (
         <section className="gallery">
-
+            {announceList}
         </section>
     )
 }
