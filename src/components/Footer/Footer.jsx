ef90179c82dff423149328dc7a3ade941331ce57
esther57-web <kamardine.ossete@gmail.com>
2024-05-31 13:50:53 +0200

    footer logo src to variables

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,17 @@
 import './footer.scss';
+import kLetterWhite from '../../assets/icons/K-white.svg';
+import logoLetterWhite from '../../assets/icons/logo-white.svg';
+import sLetterWhite from '../../assets/icons/S-white.svg';
+import aLetterWhite from '../../assets/icons/A-white.svg';
 
 function Footer() {
     return (
         <footer>
             <div>
-                <img src="./src/assets/icons/K-white.svg" alt="logo-K-white-letter"></img>
-                <img src="./src/assets/icons/logo-white.svg" alt="logo-white"></img>
-                <img src="./src/assets/icons/S-white.svg" alt="logo-S-white-letter"></img>
-                <img src="./src/assets/icons/A-white.svg" alt="logo-A-white-letter"></img>
+                <img src={kLetterWhite} alt="logo-K-white-letter"></img>
+                <img src={logoLetterWhite} alt="logo-white"></img>
+                <img src={sLetterWhite} alt="logo-S-white-letter"></img>
+                <img src={aLetterWhite} alt="logo-A-white-letter"></img>
             </div>
             <p>© 2020 Kasa. All rights reserved</p>
         </footer>
