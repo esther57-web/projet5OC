b2d05554450362e0061da6e8b287765536b9cd83
esther57-web <kamardine.ossete@gmail.com>
2024-06-04 11:55:21 +0200

    redirecting when false id announce

--- src/pages/Announces/Announces.jsx
+++ src/pages/Announces/Announces.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { AnnounceContext } from "../../Context/Context";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import AnnounceDisplay from "../../components/AnnounceDisplay/AnnounceDisplay";
 
 function Announces() {
     const {data} = useContext(AnnounceContext)
     const {announceId} = useParams()
     const announce = data.find((e)=> e.id === announceId)
-    
+
+     if (!announce) {
+          return <Navigate to={"*"}/>
+     }
     
     return (
        <main className="announce-section">
